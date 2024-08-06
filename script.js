a87cf8ff2186e47be21e1b7ffd90a71c321dfc93
Philip Gonzalez <p-gonzo@users.noreply.github.com>
2024-08-05 21:05:17 -0600

    Update font sizes

--- script.js
+++ script.js
@@ -6,10 +6,10 @@
 
         const myFunction = (x) => {
           if (x.matches) {
-              document.getElementsByClassName("post-body entry-content")[0].style.fontSize = '18px';
+              document.getElementsByClassName("post-body entry-content")[0].style.fontSize = '1em';
               document.getElementsByClassName('main_header_elements container')[0].style.overflowY = 'hidden';
           } else {
-              document.getElementsByClassName("post-body entry-content")[0].style.fontSize = '20px';
+              document.getElementsByClassName("post-body entry-content")[0].style.fontSize = '1.4em';
             }
         }
         var x = window.matchMedia("(max-width: 968px)");
