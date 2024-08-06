ba661366f920061a57827fde21da7b0b34241c54
Philip Gonzalez <p-gonzo@users.noreply.github.com>
2024-08-05 20:53:14 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+// IIFE to execute after DOM is rendered
+(() => {
+    setTimeout( () => {
+        document.getElementsByClassName('blogger')[0].style.display = 'none';
+        document.getElementsByClassName('copyright')[0].innerHTML = `&copy; Phil Gonzalez | ${new Date().getFullYear()}`;
+
+        const myFunction = (x) => {
+          if (x.matches) {
+              document.getElementsByClassName("post-body entry-content")[0].style.fontSize = '18px';
+              document.getElementsByClassName('main_header_elements container')[0].style.overflowY = 'hidden';
+          } else {
+              document.getElementsByClassName("post-body entry-content")[0].style.fontSize = '20px';
+            }
+        }
+        var x = window.matchMedia("(max-width: 968px)");
+        myFunction(x);
+        x.addEventListener("change", function() {
+          myFunction(x);
+        });
+    }, 0);
+})();
